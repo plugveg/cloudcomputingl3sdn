2897ec7e6dc983a800803e663c1b391c3417cd69
Setsuma <plug.veg@gmail.com>
2023-05-16 11:36:36 +0200

    feat(): index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ma page web</title>
+</head>
+<body>
+    <h1>Bienvenue sur ma page web!</h1>
+
+    <?php
+    // Code PHP ici
+    ?>
+
+</body>
+</html>
