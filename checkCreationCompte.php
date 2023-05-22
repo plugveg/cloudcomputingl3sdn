c3a9b5db0607eed2209efa16162e08de251abc40
Setsuma <plug.veg@gmail.com>
2023-05-22 12:38:26 +0200

    a

--- checkCreationCompte.php
+++ checkCreationCompte.php
@@ -1,16 +1,14 @@
 <?php
 function call_bdd()
 {
-    // On essaye d'appeler la Base de Donnée
+    // On essaye d'appeler la Base de Données
     try {
         $bdd = new PDO(
-            mysql:host=c.bddyar2023.postgres.database.azure.com,
-            port=5432,
-            dbname=citus,
-            user=citus,
-            password=bddyar2023!,
-            sslmode=require,
-            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+            'mysql:host=c.bddyar2023.postgres.database.azure.com;port=5432;dbname=citus',
+            'citus',
+            'bddyar2023!',
+            array(PDO::MYSQL_ATTR_SSL_CA => 'BaltimoreCyberTrustRoot.crt.pem'),
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
         );
     } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
@@ -19,33 +17,28 @@ function call_bdd()
     return $bdd;
 }
 
-// On essaie d'appeler la Base de Donnée
+// On essaie d'appeler la Base de Données
 $bdd = call_bdd();
 
-?>
-
-<body>
-    <?php
-    // On test si les valeurs entrées dans le formulaire ne sont pas nul
-    if (isset($_POST['identifiant']) && $_POST['identifiant'] != "" && isset($_POST['mot_de_passe']) && $_POST['mot_de_passe'] != "" && isset($_POST['prenom']) && $_POST['prenom'] != "" && isset($_POST['nom']) && $_POST['nom'] != "") {
-        if ($_POST['mot_de_passe'] != $_POST['verification_mot_de_passe']) {
-            header('Location: ./index.php?erreur=' . 'Mot de passe différent' . '');
-            die();
-        }
-        $identifiant_compte = $_POST['identifiant'];
-        $mot_de_passe_compte = $_POST['mot_de_passe'];
-        $prenom_compte = $_POST['prenom'];
-        $nom_compte = $_POST['nom'];
-
-        try {
-            $req = $bdd->prepare("INSERT INTO administrateur(admin_identifiant, admin_mot_de_passe, admin_prenom, admin_nom) VALUES (:identifiant, :mot_de_passe, :prenom, :nom)");
-            $req->execute(array('identifiant' => $identifiant_compte, 'mot_de_passe' => password_hash($_POST['mot_de_passe'], PASSWORD_BCRYPT), 'prenom' => $prenom_compte, 'nom' => $nom_compte));
-        } catch (Exception $e) {
-            header('Location: ./index.php?erreur=' . $e->getMessage() . '');
-            die();
-        }
-
-        header('Location: ./index.php');
+// On teste si les valeurs entrées dans le formulaire ne sont pas nulles
+if (isset($_POST['identifiant']) && $_POST['identifiant'] != "" && isset($_POST['mot_de_passe']) && $_POST['mot_de_passe'] != "" && isset($_POST['prenom']) && $_POST['prenom'] != "" && isset($_POST['nom']) && $_POST['nom'] != "") {
+    if ($_POST['mot_de_passe'] != $_POST['verification_mot_de_passe']) {
+        header('Location: ./index.php?erreur=' . urlencode('Mot de passe différent'));
+        die();
     }
-    ?>
-</body>
+    $identifiant_compte = $_POST['identifiant'];
+    $mot_de_passe_compte = $_POST['mot_de_passe'];
+    $prenom_compte = $_POST['prenom'];
+    $nom_compte = $_POST['nom'];
+
+    try {
+        $req = $bdd->prepare("INSERT INTO administrateur(admin_identifiant, admin_mot_de_passe, admin_prenom, admin_nom) VALUES (:identifiant, :mot_de_passe, :prenom, :nom)");
+        $req->execute(array('identifiant' => $identifiant_compte, 'mot_de_passe' => password_hash($_POST['mot_de_passe'], PASSWORD_BCRYPT), 'prenom' => $prenom_compte, 'nom' => $nom_compte));
+    } catch (Exception $e) {
+        header('Location: ./index.php?erreur=' . urlencode($e->getMessage()));
+        die();
+    }
+
+    header('Location: ./index.php');
+}
+?>
